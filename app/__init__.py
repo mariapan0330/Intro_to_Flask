48b1fad4e0f9aeb72ac0976036e81e51f21a7c1d
mariapan0330 <maria.pan0330@gmail.com>
2022-08-16 14:07:54 -0500

    remade virtual environment

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 
-app1 = Flask(__name__) 
+app = Flask(__name__) 
+app.config["SECRET_KEY"] = 'you-will-never-guess'
 # if you do app = Flask(__name__, render_name="abc123") then it will look for a folder called abc123 instead of 'templates'
 
 from . import routes
--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,8 @@
-from app import app1
+from app import app
 from flask import render_template
+from app.forms import SignUpForm
 
-@app1.route('/')
+@app.route('/')
 def index():
     user_info = {
         'username':'mariapan',
@@ -12,6 +13,13 @@ def index():
     # render_template takes in any number of arguments BY KEYWORD
 
 
-@app1.route('/signup')
+@app.route('/signup', methods=['GET','POST'])
 def signup():
-    return render_template('signup.html')
+    form = SignUpForm() # but you have to use a secret key!
+    # if the form is submitted and all the data is valid
+    if form.validate_on_submit(): # will return True or False
+        # if this doesn't work, it might be because you forgot csrf token.
+        print("Form has been successfully validated!!! Yay.")
+        print(form.email.data, form.username.data, form.password.data)
+        # the .data prints out whatever they submitted
+    return render_template('signup.html', form=form)
